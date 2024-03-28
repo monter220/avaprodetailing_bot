3bcd68b67c5d8c95d9e09550b12598fd7dea97e7
monter220 <bdstomsk@gmail.com>
2024-03-29 00:18:43 +0700

    Добавил дополнительную возможность в CRUD юзера

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,5 +1,25 @@
+from typing import Optional
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
 from app.models import User
 
 
-user_crud = CRUDBase(User)
+class CRUDUser(CRUDBase):
+
+    async def phone_number_exist(
+            self,
+            phone: str,
+            session: AsyncSession,
+    ) -> Optional[int]:
+        db_user_id = await session.execute(
+            select(User.id).where(
+                User.phone == phone
+            )
+        )
+        return db_user_id.scalars().first()
+
+
+user_crud = CRUDUser(User)
