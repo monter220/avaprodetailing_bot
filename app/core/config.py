1c85c61c28c28dcb5d8890d1f5486f9ef01b7b2d
monter220 <bdstomsk@gmail.com>
2024-03-30 16:14:15 +0700

    1 - добавлен раздел core/managment для утилит и вспомогательного кода 2 - добавлен файл utils.py для реализации создания токена jwt по tg_id 3 - добавлен ендпоинт в блок users для получения токена

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,12 @@ class Settings(BaseSettings):
     database_url: str
     role_list: str = '{1:"client",2:"administrator",3:"superuser"}'
 
+    access_token_expire_minutes: int = 30
+    refresh_token_expire_minutes: int = 10080
+    hash_algorithm: str = 'HS256'
+    jwt_secret_key: str = 'JWT_SECRET_KEY'
+    jwt_refresh_secret_key: str = 'JWT_REFRESH_SECRET_KEY'
+
     class Config:
         env_file = '.env'
 
