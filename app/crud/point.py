1df831ba5bf0a9d6361d157253a0124171955e4c
Сергей Иванов <isv1601@yandex.ru>
2024-04-06 13:27:19 +0300

    Эндпоинты GET, POST, PATCH, DELETE по модели Point

--- app/crud/point.py
+++ app/crud/point.py
@@ -10,36 +10,6 @@ from app.models import Point
 class CRUDPoint(CRUDBase):
 
     @staticmethod
-    async def check_unique_name(
-        field_name: str,
-        session: AsyncSession,
-    ) -> Union[None, Boolean]:
-        exists_criteria = (
-            select(Point).where(
-                Point.name == field_name
-            ).exists()
-        )
-        db_field_exists = await session.scalars(
-            select(True).where(exists_criteria)
-        )
-        return db_field_exists.first()
-
-    @staticmethod
-    async def check_unique_address(
-        field_name: str,
-        session: AsyncSession,
-    ) -> Union[None, Boolean]:
-        exists_criteria = (
-            select(Point).where(
-                Point.address == field_name
-            ).exists()
-        )
-        db_field_exists = await session.scalars(
-            select(True).where(exists_criteria)
-        )
-        return db_field_exists.first()
-
-    @staticmethod
     async def check_unique_field(
         field_name: str,
         field_value: str,
