747898fc746113c221455c30e19b1fa44320df2a
monter220 <bdstomsk@gmail.com>
2024-04-16 00:59:27 +0700

    реализовано отображение точек на странице клиента

--- app/api/endpoints/point/point.py
+++ app/api/endpoints/point/point.py
@@ -68,12 +68,15 @@ async def get_point(
         session=session,
         obj_id=point_id
     )
-
+    from fastapi.encoders import jsonable_encoder
     # TODO: В контексте так же нужно передавть "point_phone_number"
     # это должен быть номер администратора,
     # привязанного к точке на данный момент
     # если у точки есть какой-то статичный
     # номер телефона - нужно отразить это в модели.
+    points = await point_crud.point_by_id(point_id, session)
+    print(jsonable_encoder(points))
+
 
     # TODO: обдумать, как это должно передаваться и отразить в шаблоне!!!
     # Так же в контексте должен передаваться
@@ -88,7 +91,7 @@ async def get_point(
             '/point/point.html',
             {
                 'request': request,
-                'point': point,
+                'point': points,
                 # 'point_phone_number': ...
                 # 'point_service_categories': ...
             }
